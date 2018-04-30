b568410fdd9936dffd1186d680b9db61675d2fb4
Colin Gillette <colingillette35@gmail.com>
2018-04-30 10:20:22 -0500

    init commit

---
+++ ArrayFunctions/ArrayFunctions/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ArrayFunctions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstButton = new System.Windows.Forms.Button();
+            this.reverseButton = new System.Windows.Forms.Button();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstButton
+            // 
+            this.firstButton.Location = new System.Drawing.Point(21, 93);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(93, 40);
+            this.firstButton.TabIndex = 0;
+            this.firstButton.Text = "First Index Of";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+            // 
+            // reverseButton
+            // 
+            this.reverseButton.Location = new System.Drawing.Point(155, 93);
+            this.reverseButton.Name = "reverseButton";
+            this.reverseButton.Size = new System.Drawing.Size(93, 40);
+            this.reverseButton.TabIndex = 1;
+            this.reverseButton.Text = "Reverse";
+            this.reverseButton.UseVisualStyleBackColor = true;
+            this.reverseButton.Click += new System.EventHandler(this.reverseButton_Click);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(290, 93);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(93, 40);
+            this.insertButton.TabIndex = 2;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 198);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.reverseButton);
+            this.Controls.Add(this.firstButton);
+            this.Name = "Form1";
+            this.Text = "Array Functions";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Button reverseButton;
+        private System.Windows.Forms.Button insertButton;
+    }
+}
+
---
+++ ArrayFunctions/ArrayFunctions/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayFunctions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int firstInstance(int[] array, int target)
+        {
+            int position = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == target)
+                {
+                    position = i;
+                }
+            }
+
+            return position;
+        }
+
+        private string reverseArray(int[] array)
+        {
+            int temporary;
+
+            string arrayString = "The reveresed array is: ";
+
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                temporary = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temporary;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                arrayString += $"{array[i]}, ";
+            }
+
+            return arrayString;
+        }
+
+        private string insert(int[] array, int addition, int index)
+        {
+            int[] copy = new int[array.Length + 1];
+            string arrayString = "The new array is: ";
+            
+            if (index != 0)
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    copy[i] = array[i];
+                }
+            }
+
+            copy[index] = addition;
+
+            if (index != copy.Length - 1)
+            {
+                for (int i = index + 1; i < copy.Length; i++)
+                {
+                    copy[i] = array[i - 1];
+                }
+            }
+
+            for (int i = 0; i < copy.Length; i++)
+            {
+                arrayString += $"{copy[i]}, ";
+            }
+
+            return arrayString;
+        }
+
+        private void firstButton_Click(object sender, EventArgs e)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int search = 5;
+
+            int index;
+
+            index = firstInstance(array1, search);
+
+            MessageBox.Show($"The first instance of {search} happens at {index}.");
+        }
+
+        private void reverseButton_Click(object sender, EventArgs e)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            string reversed;
+
+            reversed = reverseArray(array1);
+
+            MessageBox.Show(reversed);
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int index = 6;
+            int target = 3;
+
+            string inserted;
+
+            inserted = insert(array, target, index);
+
+            MessageBox.Show(inserted);
+        }
+    }
+}
